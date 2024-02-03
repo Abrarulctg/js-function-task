3219b37a5c974af321dcd20ca1fcedc791ad1a56
Abrarulctg <jubairctg99@gmail.com>
2024-02-04 00:31:17 +0600

    Started task

---
+++ task-1.js
@@ -0,0 +1,11 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiflyNum(num1, num2, num3, num4) {
+    console.log(num1, num2, num3, num4);
+    const result = num1 * num2 * num3 * num4;
+    return result;
+}
+
+const result = multiflyNum(2, 3, 4, 5)
+
+console.log(result)
